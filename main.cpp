155197b3bf695c60901a667a4bd7485749a82f55
parse-hardik <f20180062@hyderabad.bits-pilani.ac.in>
2021-03-09 21:26:47 +0530

    Restructuring-2

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ LRPS STRIPES(vector<Edge> V,Interval x_ext){
     int xm= (V[0].coord + V[V.size()-1].coord)/2;
     vector<Edge> V1,V2;
 
-    //Dividing
+    //Divide
     for(int i=0;i<V.size();i++){
         if(V[i].coord <=xm)
             V1.push_back(V[i]);
@@ -112,91 +112,46 @@ LRPS STRIPES(vector<Edge> V,Interval x_ext){
             R2MinusLR.push_back(R2[i]);
     }
 
-                //auto Renditerator = set_union(R2MinusLR.begin(),R2MinusLR.end(), R1.begin(),R1.end(), R.begin(),compareInterval); 
-                for(int i=0; i<R2MinusLR.size();i++)
-                {
-                    R.push_back(R2MinusLR[i]);
-                }
-                for(int i=0;i<R1.size();i++)
-                {
-                    bool found=false;
-                    for(int j=0;j<R.size();j++)
-                    {
-                        if(R1[i].bottom == R[j].bottom && R1[i].top ==R[j].top)
-                        { 
-                            found=true;
-                            break;
-                        }
-                    }
-                    if(!found)
-                        R.push_back(R1[i]);
-                }  
-                cout<<"step 3\n";
+    for(int i=0; i<R2MinusLR.size();i++){
+        R.push_back(R2MinusLR[i]);
+    }
+
+    for(int i=0;i<R1.size();i++){
+        bool found=false;
+        for(int j=0;j<R.size();j++){
+            if(R1[i].bottom == R[j].bottom && R1[i].top ==R[j].top)
+            { 
+                found=true;
+                break;
+            }
+        }
+        if(!found)
+            R.push_back(R1[i]);
+    }  
   
     //P=P1 U P2
-        //auto Penditerator = set_union(P1.begin(),P1.end(), P2.begin(),P2.end(), P.begin()); 
-        for(int i=0; i<P1.size();i++)
-        {
-            P.push_back(P1[i]);
-        }
-        for(int i=0;i<P2.size();i++)
-        {
-            if(find(P.begin(),P.end(),P2[i])==P.end())
-                P.push_back(P2[i]);
-        }
-        cout<<"step 4\n";
+    for(int i=0; i<P1.size();i++){
+        P.push_back(P1[i]);
+    }
+
+    for(int i=0;i<P2.size();i++){
+        if(find(P.begin(),P.end(),P2[i])==P.end())
+            P.push_back(P2[i]);
+    }
 
     //Sleft : = copy (S1, P, [xext.bottom, x,,]); 
     //Sright: = copy (S2, P, [xm, xext.top]) 
     vector<Stripe> Sleft,Sright;
+
     Sleft = copy(LRPS1.S , P , {x_ext.bottom , xm});
-    cout<<"going to next copy\n";
     Sright = copy(LRPS2.S , P , { xm, x_ext.top});
 
-    cout<<"before blacken in sleft\n";
-    for(auto stripe : Sleft)
-    {
-        cout<<"Y: "<<stripe.y_inter.bottom<<" "<<stripe.y_inter.top<<" "<<"X: "<<stripe.x_inter.bottom<<" "<<stripe.x_inter.top<<"\n";
-        for(auto color : stripe.x_union)
-        {
-            cout<<color.bottom<<' '<<color.top<<" \n";
-        }
-    }
-    cout<<"before blacken in sright\n";
-    for(auto stripe : Sright)
-    {
-        cout<<"Y: "<<stripe.y_inter.bottom<<" "<<stripe.y_inter.top<<" "<<"X: "<<stripe.x_inter.bottom<<" "<<stripe.x_inter.top<<"\n";
-        for(auto color : stripe.x_union)
-        {
-            cout<<color.bottom<<' '<<color.top<<" \n";
-        }
-    }
     //blacken (Sle it, R 2 \ LR ); 
     //blacken ( Sright, L I\LR); 
     Sleft = blacken(Sleft , R2MinusLR);
     Sright = blacken(Sright , L1MinusLR);
 
-    cout<<"before concat in sleft\n";
-    for(auto stripe : Sleft)
-    {
-        cout<<"Y: "<<stripe.y_inter.bottom<<" "<<stripe.y_inter.top<<" "<<"X: "<<stripe.x_inter.bottom<<" "<<stripe.x_inter.top<<"\n";
-        for(auto color : stripe.x_union)
-        {
-            cout<<color.bottom<<' '<<color.top<<" \n";
-        }
-    }
-    cout<<"before concat in sright\n";
-    for(auto stripe : Sright)
-    {
-        cout<<"Y: "<<stripe.y_inter.bottom<<" "<<stripe.y_inter.top<<" "<<"X: "<<stripe.x_inter.bottom<<" "<<stripe.x_inter.top<<"\n";
-        for(auto color : stripe.x_union)
-        {
-            cout<<color.bottom<<' '<<color.top<<" \n";
-        }
-    }
-
     S= concat(Sleft,Sright, P,x_ext);
-    cout<<"returning from main\n";
     return {L,R,P,S};
 }
 
